ae9ef4b9980815ea9fc04fc93473106a573587eb
Nils Thielke <galfar@gmx.de>
2017-06-08 20:45:44 +0200

    project base

---
+++ javaAnalyser/javaanalyser/src/main/java/com/github/javaanalyser/javaanalyser/JavaAnalyser.java
@@ -0,0 +1,13 @@
+package com.github.javaanalyser.javaanalyser;
+
+
+import javax.enterprise.event.Observes;
+import org.jboss.weld.environment.se.events.ContainerInitialized;
+
+public class JavaAnalyser {
+
+	public void main(@Observes ContainerInitialized event) {
+		
+	}
+	
+}
